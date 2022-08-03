6cd413dff6500436327afbb310633fa975240eef
ruslana-tymchyk <tymchyk3@gmail.com>
2022-08-03 19:48:03 +0100

    instructions refined, version test2.0

--- public/js/saveData.js
+++ public/js/saveData.js
@@ -1,13 +1,4 @@
-//use version number to separate itterations of experiment
-//the only commands - set or update -> need to set it first, then update
-//collecton -> document -> collection ->document
-//update and set documents but not collections
-//two collections - one for task and one for questionnaires 
-//by setting you create a collection in the first place - then put whatever you want in it
-//write data after every trial -> does not slow down the experiment
-//everything is stored in key/value format
-//savetask data takes index and data to save 
-// enable persistence 
+
 firebase.firestore().enablePersistence()
 .catch(function(err) {
   if (err.code == 'failed-precondition') {  // multiple tabs open, persistence can only be enabled in one tab at a a time
@@ -17,9 +8,10 @@ firebase.firestore().enablePersistence()
 
 // initialize db
 var db = firebase.firestore();
+var version = "test2.0"
 // function to save consent 
 var saveConsent = function(){
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).set({
+  db.collection(version).doc('mind').collection('minddata').doc(uid).set({
     firebaseUID: uid,             // firebase user ID 
     //prolificSubID: subjectID,     // prolific subject ID 
     //prolificStudyID: studyID,     // prolific study ID 
@@ -29,16 +21,16 @@ var saveConsent = function(){
     participantOS: navigator.userAgent
   }); 
   // initialize data-storage collections
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('own_profile').set({init: 1});
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('rank_profiles').set({init: 1});
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('main_task').set({init: 1});
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('mood_rating').set({init: 1});
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('viewing_time').set({init: 1});
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('post_task_data').doc('questionnaires').set({init: 1});
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('own_profile').set({init: 1});
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('rank_profiles').set({init: 1});
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('main_task').set({init: 1});
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('mood_rating').set({init: 1});
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('viewing_time').set({init: 1});
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('post_task_data').doc('questionnaires').set({init: 1});
 };
 
 var savePreTaskData = function(response){
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('own_profile').update({
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('own_profile').update({
     own_profile: response,
     //own_profile_rt: rt, -maybe add this       
 
@@ -46,44 +38,29 @@ var savePreTaskData = function(response){
 };
 
 var saveProfileRatingsData = function(profile_count, respData, respRT){
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('rank_profiles').update(
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('pre_task_data').doc('rank_profiles').update(
     {[profile_count]: respData,
       rt: respRT}  
 
   ); 
 };
 
-//Still dunno if I actually need this thing
-/*
-var saveRankTestData = function(rank_test){
-  console.log('at least I am here');
-  db.collection('tasks').doc('mind').collection('minddata').doc('uid').collection('pre_task_data').doc('rank_test').set({
-    rank_test_data: rank_test 
-  }); 
-};
-var savePilotRun = function(cond){
-  db.collection('tasks').doc('mind').collection('minddata').doc('uid').collection('pre_task_data').doc('condition').set({
-    condition_code: cond 
-  }); 
-};
-*/
-
 var saveTaskData = function(trialN, response){
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('main_task').update(
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('main_task').update(
     {[trialN]: response}  
 
   ); 
 };
 
 var saveMoodData = function(trialN, response){
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('mood_rating').update(
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('mood_rating').update(
     {[trialN]: response}  
 
   ); 
 };
 
 var saveQuestData = function(questionnaire, dataToSave, completionRT) {
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('post_task_data').doc('questionnaires').update({
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('post_task_data').doc('questionnaires').update({
     [questionnaire]: dataToSave,
     [questionnaire+'_RT']: completionRT
   });
@@ -92,18 +69,19 @@ var saveQuestData = function(questionnaire, dataToSave, completionRT) {
 
 
 var saveViewTime = function(view_history, rt, condition){
-  db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('viewing_time').update(
+  db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('viewing_time').update(
     {view: view_history,
-    reaction: rt,
-    cond: condition
+    rt: rt,
+    condition: condition
   }  
   ); 
 };
 
+/*
 
 var getMoodRating = function(){
 
-        var docRef = db.collection('tasks').doc('mind').collection('minddata').doc(uid).collection('task_data').doc('mood_rating');
+        var docRef = db.collection(version).doc('mind').collection('minddata').doc(uid).collection('task_data').doc('mood_rating');
 
         docRef.get().then((doc) => {
             if (doc.exists) {
@@ -119,6 +97,7 @@ var getMoodRating = function(){
         });
   }
 
+*/
 
-export { saveConsent, savePreTaskData, saveProfileRatingsData, saveTaskData, saveMoodData, saveQuestData, saveViewTime, getMoodRating};
+export { saveConsent, savePreTaskData, saveProfileRatingsData, saveTaskData, saveMoodData, saveQuestData, saveViewTime};
 
